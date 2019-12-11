5b6bb43334f42c04cc83d75157f719d98c834158
Laptop <datasup12@gmail.com>
2019-12-11 23:05:41 +0700

    new file:   laptop/progs/o61_oct_c1_knives.cpp

---
+++ laptop/progs/o61_oct_c1_knives.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int X = 1e6 + 5;
+int N[2], L, Q, cur[X], st[2][X];
+bool fl[X];
+void ud(bool ty, int po, int va){ for(++po; po <= L; po += -po & po) st[ty][po] += va; }
+int gt(bool ty, int po, int ret = 0){ for(++po; po; po -= -po & po) ret += st[ty][po]; return ret;}
+long long sum;
+int main(){
+	ios::sync_with_stdio(0), cin.tie(0);
+	cin >> N[0] >> L >> Q;
+	ud(0, 0, N[0]);
+	for(int z = 0, u, v, nw; z < Q; ++z){
+		string ty; cin >> ty >> u;
+		if(ty[0] == 's'){
+			cin >> v;
+			nw = (cur[u] + L + v) % L;
+			ud(fl[u], cur[u], -1), ud(fl[u], nw, 1);
+			sum += (fl[u]? -1 : 1) * (nw - cur[u]), cur[u] = nw;
+		} else if(ty[0] == 'f'){
+			ud(fl[u], cur[u], -1), --N[fl[u]];
+			sum += (fl[u]? 2 : -2) * cur[u]; 
+			fl[u] ^= 1; 
+			ud(fl[u], cur[u], 1), ++N[fl[u]];
+		} else{
+			cout << 
+			1ll * L * (N[0] - gt(0, max(u - 1, 0)) + gt(1, u)) + 
+			(1ll * u * (N[0] - N[1]))
+			- sum << '\n';
+		}
+	}
+}
