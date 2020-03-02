ce30d44bdf52bc04d42d6879eb9f05ded67937b5
ZikXewen <datasup12@gmail.com>
2020-03-02 18:50:03 +0700

    modified:   Desktop/o60_may10_mafiameeting.cpp

--- Desktop/o60_may10_mafiameeting.cpp
+++ Desktop/o60_may10_mafiameeting.cpp
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
+#define vi vector<int>
 using namespace std;
-const int X = 1e5 + 5;
-int N, M, Q, ct, low[X], pre[X], anc[X][(int) log2(X)], lv[X];
-bool ap[X];
-vector<vector<int>> g(X), cc;
-vector<int> sz;
+const int X = 2e5 + 5;
+int N, M, Q, ct, low[X], pre[X], anc[X][(int) log2(X)], lv[X], ds[X], id[X];
+bitset<X> ap, vs;
+vector<vi> g(X), cc;
+vi sz;
 stack<int> stk;
 void grp(int u = 1, int p = 0){
 	low[u] = pre[u] = ++ct;
-	stk.emplace(u)
+	stk.emplace(u);
 	for(int v: g[u]){
 		if(!pre[v]){
 			grp(v, u);
 			low[u] = min(low[u], low[v]);
-			if(low[v] >= pre[u]){
+			if(low[v] >= pre[u] || (!p && g[u].size() > 1)){
 				ap[u] = 1;
 				cc.emplace_back(vi(1, u));
-				do cc.back().emplace_back(stk.top()), stk.pop(); while(stk.top() != v);
+				while(cc.back().back() != v) cc.back().emplace_back(stk.top()), stk.pop();
 			}
 		} else if(v != p) low[u] = min(low[u], pre[v]);
 	}
@@ -25,17 +26,22 @@ void bct(){
 	g.clear(); ct = 0;
 	g.emplace_back(vi());
 	sz.emplace_back(0);
-	for(int i = 1; i <= N; ++i) if(ap[i]) id[i] = ++ct, g.emplace_back(vi()), sz.emplace_back(1);
+	for(int i = 1; i <= N; ++i) if(ap[i]) id[i] = ++ct, g.emplace_back(vi()), sz.emplace_back(-1);
 	for(vi i: cc){
 		g.emplace_back(vi()), sz.emplace_back(i.size()); ++ct;
 		for(int v: i)
-			if(ap[v]) g[ct].emplace_back(id[v]), g[id[v]].emplace_back(ct)
+			if(ap[v]) g[ct].emplace_back(id[v]), g[id[v]].emplace_back(ct);
 			else id[v] = ct;
 	}
 }
+void dfs(int u = 1, int p = 0){
+	vs[u] = 1;
+	for(int v: g[u]) if(!vs[v]) ds[v] = ds[u] + sz[v], dfs(v, u);
+}
 void plca(int u = 1, int p = 0){
-	for(int i = 1; i <= log2(ct); ++i) ans[u][i] = anc[anc[u][i - 1]][i - 1];
-	for(int v: g[u]) if(v != p) ans[v][0] = u, lv[v] = lv[u] + 1, plca(v, u);
+	anc[u][0] = p, lv[u] = lv[p] + 1;
+	for(int i = 1; i <= log2(ct); ++i) anc[u][i] = anc[anc[u][i - 1]][i - 1];
+	for(int v: g[u]) if(v != p) plca(v, u);
 }
 int lca(int u, int v){
 	if(lv[u] < lv[v]) swap(u, v);
@@ -44,10 +50,15 @@ int lca(int u, int v){
 	for(int i = log2(ct); i >= 0; --i) if(anc[u][i] != anc[v][i]) u = anc[u][i], v = anc[v][i];
 	return anc[u][0];
 }
+int gt(int u){return ds[u] + (sz[u] == -1);}
+int calc(int u, int v){
+	int w = lca(u, v);
+	return gt(u) + gt(v) - (gt(w) << 1) + abs(sz[w]) - 2;
+}
 int main(){
 	ios::sync_with_stdio(0), cin.tie(0);
 	cin >> N >> M >> Q;
 	for(int i = 0, u, v; i < M; ++i) cin >> u >> v, g[u].emplace_back(v), g[v].emplace_back(u);
-	grp(); bct(); plca();
-	for(int i = 0, u, v; i < Q; ++i) cin >> u >> v, cout << lca(id[u], id[v]);
+	grp(); bct(); vs[0] = 1, ds[1] = sz[1], dfs(); plca();
+	for(int i = 0, u, v; i < Q; ++i) cin >> u >> v, cout << calc(id[u], id[v]) << '\n';
 }
