36146ae1d769f4dc669f4a1cbb9fa4d03ac6f7e7
ZikXewen <datasup12@gmail.com>
2020-06-08 14:08:56 +0700

    080620-1

--- laptop/thailandoi/o59_may11_sum.cpp
+++ laptop/thailandoi/o59_may11_sum.cpp
@@ -6,21 +6,17 @@ const long INF = 1e11 + 5;
 int N, tr[X];
 long K, ar[X];
 vector<long> co;
-int _id(long u){
-    int ret = upper_bound(co.begin(), co.end(), u) - co.begin(); //care
-    return ret; // care
-}
+int _id(long u){ return upper_bound(co.begin(), co.end(), u) - co.begin(); }
 void ud(long id){for(id = _id(id); id < X; id += -id & id) ++tr[id];}
 int gt(long id){int ret = 0; for(id = _id(id); id; id -= -id & id) ret += tr[id]; return ret;}
 long ck(long in){
     long ret = 0;
     memset(tr, 0, sizeof tr);
     for(int i = 1; i <= N; ++i) ud(ar[i - 1]), ret += gt(ar[i] - in);
-    // cout << in << ' ' << _id(in) << ' ' << ret << '\n';
     return ret;
 }
 int main(){
-   // ios::sync_with_stdio(0), cin.tie(0);
+    ios::sync_with_stdio(0), cin.tie(0);
     cin >> N >> K;
     for(int i = 1; i <= N; ++i) cin >> ar[i], co.emplace_back(ar[i] += ar[i - 1]);
     co.emplace_back(0); sort(co.begin(), co.end());
@@ -32,6 +28,4 @@ int main(){
         else r = m - 1;
     }
     cout << l;
-    // for(auto i: co) cout << i;
-//    cout << _id(4);
 }
