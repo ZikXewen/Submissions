2fc160d7d9bfca6fff2d1f4fd529823c919d46db
ZikLen <datasup12@gmail.com>
2020-06-14 22:10:11 +0700

    modified:   cf_100551B.cpp

--- laptop/Codeforces/cf_100551B.cpp
+++ laptop/Codeforces/cf_100551B.cpp
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 #define ii pair<int, int>
 using namespace std;
-const int X = 5e5 + 5, Y = 1e5 + 5;
+const int X = 1e5 + 5;
 
-int N, M, K, l[X];
-bool an[Y];
-ii g[Y];
-vector<pair<int, ii>> rh[Y], lh[Y]; // [l, r)
+vector<pair<int, ii>> rh[X], lh[X];
 
 struct one{
-    int p[Y], cr;
+    int cr;
+    vector<int> an, p;
     stack<ii> stk;
 
-    one(){ iota(p, p + Y, 0), cr = 0; }
+    one(int _sz, int _sa) : p(vector<int>(_sz + 1, -1)), cr(0), an(vector<int>(_sa + 1)) {}
     int chn(int u, int v){
         stk.emplace(u, p[u]);
         return p[u] = v;
     }
-    int fd(int u){return chn(u, p[u] == u? u : fd(p[u]));}
+    int fd(int u){return p[u] < 0? u : fd(p[u]);}
     bool un(ii nw){
         auto [u, v] = nw; u = fd(u), v = fd(v);
         if(u == v) return 0;
-        chn(u, v);
+        if(p[u] > p[v]) swap(u, v);
+        chn(v, p[v] + p[u]); chn(u, v);
         ++cr;
         return 1;
     }
@@ -31,24 +30,30 @@ struct one{
         }
     }
     void bd(int l, int r){
-        if(l == r) return void(an[l] = (cr == N - 1));
+        if(l == r) return void(an[l] = (cr == p.size() - 2));
         int torb = stk.size(), tocr = cr;
         int m = (l + r) >> 1;
-        for(int i = l; i <= m; ++i) for(auto [u, va]: lh[i]) if(u >= r) un(va);
-        bd(l, m);
-        cr = tocr; rb(torb);
-        for(int i = m + 1; i <= r; ++i) for(auto [u, va]: rh[i]) if(u <= l) un(va);
+        for(int i = l; i <= m + 1; ++i) for(auto [u, va]: lh[i]) if(u >= r) un(va);
         bd(m + 1, r);
         cr = tocr; rb(torb);
+        for(int i = m; i <= r; ++i) for(auto [u, va]: rh[i]) if(u <= l) un(va);
+        bd(l, m);
+        cr = tocr; rb(torb);
     }
-} dsu;
+    void prt(){ for(int i = 1; i < an.size(); ++i) cout << (an[i]? "Connected" : "Disconnected") << '\n';}
+};
+
+int N, M, K, l[X];
+ii g[X];
 
 int main(){
     ios::sync_with_stdio(0), cin.tie(0);
+    freopen("inp.txt", "r", stdin);
     cin >> N >> M;
     fill(l, l + M + 1, 1);
     for(int i = 1, u, v; i <= M; ++i) cin >> u >> v, g[i] = ii(u, v);
     cin >> K;
+    one dsu = one(N, K);
     for(int i = 1, u; i <= K; ++i) {
         cin >> u;
         for(int j = 0, v; j < u; ++j) {
@@ -57,8 +62,8 @@ int main(){
             else lh[l[v]].emplace_back(i - 1, g[v]), rh[i - 1].emplace_back(l[v], g[v]), l[v] = i + 1;
         }
     }
-    for(int i = 0; i < N; ++i) if(l[i])
+    for(int i = 1; i <= M; ++i) if(l[i] <= K)
         lh[l[i]].emplace_back(K, g[i]), rh[K].emplace_back(l[i], g[i]);
     dsu.bd(1, K);
-    for(int i = 0; i < K; ++i) cout << an[i];
+    dsu.prt();
 }
