b8fb4c4421e08e91be7241ddb24abec1c8cde741
ZikLen <datasup12@gmail.com>
2020-06-19 00:19:20 +0700

    new file:   pbdsNote.cpp

---
+++ Desktop/pbdsNote.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template<typename T> 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
